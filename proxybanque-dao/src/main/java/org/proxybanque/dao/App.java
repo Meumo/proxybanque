603d4f7d7f1844225b6fe5753958175a3430a753
Mamadou DIOUF <mamadou-d@hotmail.com>
2018-12-05 12:26:49 +0000

    first commit

---
+++ proxybanque-dao/src/main/java/org/proxybanque/dao/App.java
@@ -0,0 +1,13 @@
+package org.proxybanque.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
