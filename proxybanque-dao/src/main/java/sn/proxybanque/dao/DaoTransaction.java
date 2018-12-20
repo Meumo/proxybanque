c96a8356fe1ed9a2389a374f7b6d7dbcb031f36b
Mamadou DIOUF <mamadou-d@hotmail.com>
2018-12-20 11:35:39 +0000

    11:35 20/12/2018

--- proxybanque-dao/src/main/java/sn/proxybanque/dao/DaoTransaction.java
+++ proxybanque-dao/src/main/java/sn/proxybanque/dao/DaoTransaction.java
@@ -3,16 +3,31 @@ package sn.proxybanque.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 import sn.proxybanque.domaine.Compte;
+import sn.proxybanque.domaine.Transaction;
 import sn.proxybanque.utils.MysqlConnection;
 
 public class DaoTransaction {
 	Connection con = MysqlConnection.getInstanceConnection();
-	Numero numero = new Numero();
 	IDaoCompteImp compteImp = new IDaoCompteImp();
+
+	public void create(Transaction t) {
+		String sql = "INSERT INTO transaction(numeroTransaction, montantTransaction, dateTransaction, typeTransaction, idCompte, idConseiller) VALUES (?,?,?,?,?,?)";
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, t.getNumeroTransaction());
+			ps.setDouble(2, t.getMontantTransaction());
+			java.sql.Date date_sql = new java.sql.Date(t.getDateTransaction().getTime());
+			ps.setDate(3, date_sql);
+			ps.setString(4, t.getTypeTransaction());
+			ps.setInt(5, t.getIdcompte());
+			ps.setInt(6, t.getIdconseiller());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public void verser(Compte compte, double montcred) {
 		double ancienSolde = compte.getSoldeCompte();
@@ -20,43 +35,17 @@ public class DaoTransaction {
 		compte.setSoldeCompte(nouvoSolde);
 
 		compteImp.update(compte);
-		String sql = "INSERT INTO transaction(numeroTransaction, montantTransaction, dateTransaction, typeTransaction, idCompte, idConseiller) VALUES (?,?,?,?,?,?)";
-		try {
-			PreparedStatement ps = con.prepareStatement(sql);
-			ps.setString(1, numero.generateNumeroTransaction());
-			ps.setDouble(2, montcred);
-			ps.setString(3, LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-			ps.setString(4, "Credit");
-			ps.setInt(5, compte.getIdCompte());
-			ps.setInt(6, 1);
-			ps.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
 		System.out.println("Operation reussie! Nouveau solde:" + nouvoSolde);
 	}
 
 	public boolean retirer(Compte compte, double montdb) {
 		boolean result = false;
 		if (compte.getSoldeCompte() > montdb) {
-
 			double ancienSolde = compte.getSoldeCompte();
 			double nouvoSolde = ancienSolde - montdb;
 			compte.setSoldeCompte(nouvoSolde);
+
 			compteImp.update(compte);
-			String sql = "INSERT INTO transaction(numeroTransaction, montantTransaction, dateTransaction, typeTransaction, idCompte, idConseiller) VALUES (?,?,?,?,?,?)";
-			try {
-				PreparedStatement ps = con.prepareStatement(sql);
-				ps.setString(1, numero.generateNumeroTransaction());
-				ps.setDouble(2, montdb);
-				ps.setString(3, LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-				ps.setString(4, "Dedit");
-				ps.setInt(5, compte.getIdCompte());
-				ps.setInt(6, 1);
-				ps.executeUpdate();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
 			System.out.println("Operation reussie! Nouveau solde:" + nouvoSolde);
 			result = true;
 			System.out.println("-----------------");
@@ -68,38 +57,12 @@ public class DaoTransaction {
 
 	public void virer(Compte compteDebiteur, Compte compteCrediteur, double montant) {
 		boolean isCorrect = this.retirer(compteDebiteur, montant);
-		String numeroTransaction = numero.generateNumeroTransaction();
 		if (isCorrect) {
 			compteImp.update(compteDebiteur);
-			String sql = "INSERT INTO transaction(numeroTransaction, montantTransaction, dateTransaction, typeTransaction, idCompte, idConseiller) VALUES (?,?,?,?,?,?)";
-			try {
-				PreparedStatement ps = con.prepareStatement(sql);
-				ps.setString(1, numeroTransaction);
-				ps.setDouble(2, -montant);
-				ps.setString(3, LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-				ps.setString(4, "Virement");
-				ps.setInt(5, compteDebiteur.getIdCompte());
-				ps.setInt(6, 1);
-				ps.executeUpdate();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
 
 			this.verser(compteCrediteur, montant);
+
 			compteImp.update(compteCrediteur);
-			String sql2 = "INSERT INTO transaction(numeroTransaction, montantTransaction, dateTransaction, typeTransaction, idCompte, idConseiller) VALUES (?,?,?,?,?,?)";
-			try {
-				PreparedStatement ps = con.prepareStatement(sql2);
-				ps.setString(1, numeroTransaction);
-				ps.setDouble(2, +montant);
-				ps.setString(3, LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-				ps.setString(4, "Virement");
-				ps.setInt(5, compteCrediteur.getIdCompte());
-				ps.setInt(6, 1);
-				ps.executeUpdate();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
 
 			System.out.println("Operation de virement reussie!!");
 		} else {
--- proxybanque-service/src/main/java/sn/proxybanque/service/Numero.java
+++ proxybanque-service/src/main/java/sn/proxybanque/service/Numero.java
@@ -1,151 +1,34 @@
 package sn.proxybanque.service;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 import sn.proxybanque.utils.MysqlConnection;
 
 public class Numero {
 	Connection con = MysqlConnection.getInstanceConnection();
-
+sn.proxybanque.dao.Numero numero =new sn.proxybanque.dao.Numero();
 	public String generateNumeroCarte() {
-		long sufCarte = 0;
-		String prefCarte = null;
-		try {
-			String sql = "SELECT * FROM numero";
-			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery(sql);
-			while (rs.next()) {
-				prefCarte = rs.getString("numeroCarte");
-				sufCarte = rs.getLong("sufCarte");
-				String req = "UPDATE numero SET sufCarte=? WHERE id=?";
-				PreparedStatement ps = con.prepareStatement(req);
-				ps.setLong(1, sufCarte + 1);
-				ps.setLong(2, 1);
-				ps.executeUpdate();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		return prefCarte + " " + sufCarte;
+		return numero.generateNumeroCarte();
 	}
 
 	public String generateNumeroCompte() {
-		long sufCompte = 0;
-		String prefCompte = null;
-		try {
-			String sql = "SELECT * FROM numero";
-			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery(sql);
-			while (rs.next()) {
-				prefCompte = rs.getString("numeroCompte");
-				sufCompte = rs.getLong("sufCompte");
-				String req = "UPDATE numero SET sufCompte=? WHERE id=?";
-				PreparedStatement ps = con.prepareStatement(req);
-				ps.setLong(1, sufCompte + 1);
-				ps.setLong(2, 1);
-				ps.executeUpdate();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		return prefCompte + " " + sufCompte;
+		return numero.generateNumeroCompte();
 	}
 
 	public String generateNumeroAgence() {
-		long sufAgence = 0;
-		long prefAgence = 0;
-		try {
-			String sql = "SELECT * FROM numero";
-			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery(sql);
-			while (rs.next()) {
-				prefAgence = rs.getLong("numeroAgence");
-				sufAgence = rs.getLong("sufAgence");
-				String req = "UPDATE numero SET sufAgence=? WHERE id=?";
-				PreparedStatement ps = con.prepareStatement(req);
-				ps.setLong(1, sufAgence + 1);
-				ps.setLong(2, 1);
-				ps.executeUpdate();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		return "" + prefAgence + sufAgence;
+		return numero.generateNumeroAgence();
 	}
 
 	public String generateNumeroClient() {
-		long sufClient = 0;
-		long prefClient = 0;
-		try {
-			String sql = "SELECT * FROM numero";
-			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery(sql);
-			while (rs.next()) {
-				prefClient = rs.getLong("numeroClient");
-				sufClient = rs.getLong("sufClient");
-				String req = "UPDATE numero SET sufClient=? WHERE id=?";
-				PreparedStatement ps = con.prepareStatement(req);
-				ps.setLong(1, sufClient + 1);
-				ps.setLong(2, 1);
-				ps.executeUpdate();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		return "" + prefClient + sufClient;
+		return numero.generateNumeroClient();
 	}
 
 	public String generateNumeroEmploye() {
-		long sufEmploye = 0;
-		long prefEmploye = 0;
-		try {
-			String sql = "SELECT * FROM numero";
-			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery(sql);
-			while (rs.next()) {
-				prefEmploye = rs.getLong("numeroEmploye");
-				sufEmploye = rs.getLong("sufEmploye");
-				String req = "UPDATE numero SET sufEmploye=? WHERE id=?";
-				PreparedStatement ps = con.prepareStatement(req);
-				ps.setLong(1, sufEmploye + 1);
-				ps.setLong(2, 1);
-				ps.executeUpdate();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		return "" + prefEmploye + sufEmploye;
+		return numero.generateNumeroEmploye();
 	}
 
 	public String generateNumeroTransaction() {
-		long sufTransaction = 0;
-		long prefTransaction = 0;
-		try {
-			String sql = "SELECT * FROM numero";
-			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery(sql);
-			while (rs.next()) {
-				prefTransaction = rs.getLong("numeroTransaction");
-				sufTransaction = rs.getLong("sufTransaction");
-				String req = "UPDATE numero SET sufTransaction=? WHERE id=?";
-				PreparedStatement ps = con.prepareStatement(req);
-				ps.setLong(1, sufTransaction + 1);
-				ps.setLong(2, 1);
-				ps.executeUpdate();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
 
-		return "" + prefTransaction + sufTransaction;
+		return numero.generateNumeroTransaction();
 	}
 }
