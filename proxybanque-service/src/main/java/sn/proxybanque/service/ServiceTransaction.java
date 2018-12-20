5e713cd99c65eed565f329b5d2894adc490923fa
Mamadou DIOUF <mamadou-d@hotmail.com>
2018-12-20 12:58:42 +0000

    12:58 20/12/2018

--- proxybanque-service/src/main/java/sn/proxybanque/service/ServiceTransaction.java
+++ proxybanque-service/src/main/java/sn/proxybanque/service/ServiceTransaction.java
@@ -2,19 +2,23 @@ package sn.proxybanque.service;
 
 import sn.proxybanque.dao.DaoTransaction;
 import sn.proxybanque.domaine.Compte;
+import sn.proxybanque.domaine.Transaction;
 
 public class ServiceTransaction {
 	DaoTransaction daoTransaction = new DaoTransaction();
 
-	public void verser(Compte compte, float montcred) {
+	public void create(Transaction t) {
+		daoTransaction.create(t);
+	}
+	public void verser(Compte compte, double montcred) {
 		daoTransaction.verser(compte, montcred);
 	}
 
-	public boolean retirer(Compte compte, float montdb) {
+	public boolean retirer(Compte compte, double montdb) {
 		return daoTransaction.retirer(compte, montdb);
 	}
 
-	public void virer(Compte compteDebiteur, Compte compteCrediteur, float montant) {
+	public void virer(Compte compteDebiteur, Compte compteCrediteur, double montant) {
 		daoTransaction.virer(compteDebiteur, compteCrediteur, montant);
 	}
 }
