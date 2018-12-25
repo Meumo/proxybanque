2360775c53bb0070c2f46cbf03b7b4c402659933
mamadoudiack <diackmo.dm@gmail.com>
2018-12-25 18:01:29 +0000

    ok

--- proxybanque-gui/src/main/java/sn/proxybanque/gui/Virement.java
+++ proxybanque-gui/src/main/java/sn/proxybanque/gui/Virement.java
@@ -108,6 +108,8 @@ public class Virement extends JPanel {
 			public void actionPerformed(ActionEvent e) {
 				ServiceTransaction serviceTransaction = new ServiceTransaction();
 				double montantAVirer = 0;
+				String numTransaction = numero.generateNumeroTransaction();
+				System.out.println(numTransaction +"sddddddddd");
 				if (montant.getText().length() > 0) {
 					montantAVirer = Double.parseDouble(montant.getText());
 				}
@@ -117,7 +119,8 @@ public class Virement extends JPanel {
 					if (JOptionPane.showConfirmDialog(null, "voulez vous confirmer le virement", "Confirmation",
 							JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
 						Transaction transaction;
-						String numTransaction = numero.generateNumeroTransaction();
+						//String numTransaction = numero.generateNumeroTransaction();
+						//System.out.println(numTransaction +"sddddddddd");
 						serviceTransaction.retirer(compteDebiteur, montantAVirer);
 						serviceTransaction.verser(compteCrediteur, montantAVirer);
 						numero = new Numero();
