d5ed30e58a1b3170847174a1fc4871f193305d98
Mamadou DIOUF <mamadou-d@hotmail.com>
2018-12-31 13:18:37 +0000

    13:18 31/12/2018

--- proxybanque-dao/src/main/java/sn/proxybanque/dao/DaoAudit.java
+++ proxybanque-dao/src/main/java/sn/proxybanque/dao/DaoAudit.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import sn.proxybanque.domaine.Transaction;
 import sn.proxybanque.utils.MysqlConnection;
 
 public class DaoAudit {
@@ -31,5 +34,30 @@ public class DaoAudit {
 			e.printStackTrace();
 		}
 		return ok;
+	}
+
+	public List<Transaction> listeTransactionNonRecommandee() {
+		List<Transaction> listTransaction = new ArrayList<Transaction>();
+		try {
+			String sql = "SELECT * FROM transaction WHERE montantTransaction>? AND typeTransaction=?";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setDouble(1, 3000000);
+			ps.setString(2, "Retrait");
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				Transaction transaction = new Transaction();
+
+				transaction.setNumeroTransaction(rs.getString("numeroTransaction"));
+				transaction.setMontantTransaction(rs.getDouble("montantTransaction"));
+				transaction.setDateTransaction(rs.getDate("dateTransaction"));
+				transaction.setTypeTransaction(rs.getString("typeTransaction"));
+				transaction.setIdcompte(rs.getInt("idCompte"));
+				transaction.setIdconseiller(rs.getShort("idConseiller"));
+				listTransaction.add(transaction);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return listTransaction;
 	}
 }
